aa7c81fe3b496ea82b1c46d9f364d2afbcb5d785
DexCruise <dexter.e.cruise@gmail.com>
2021-10-09 21:42:13 -0700

    added hello world program, and expanded .gitignore to include compile outputs.
    compiler now functions on outputs, and thus can compile the hello world program!

--- compiler.py
+++ compiler.py
@@ -1,16 +1,17 @@
 # compiles English to python.
 import sys
+
 args = sys.argv
 
 english_file = open(args[-1], "r").read()
-
+output = open(args[-1] + ".output.py", "w")
 
 quote_tokens = []
 for i in zip(english_file, range(len(english_file))):
     if i[0] == '"':
         quote_tokens += [i[1]]
 
-print(quote_tokens)
+print("quote tokens:", quote_tokens)
 quotations = []
 while quote_tokens:
     start = quote_tokens.pop(0)
@@ -22,9 +23,49 @@ while quote_tokens:
     else:
         quotations += [english_file[start + 1:end:]]
         english_file = \
-            english_file[0:start] + "string_" + str(len(quotations) - 1) + english_file[end + 1:len(english_file)]
+            english_file[0:start] + "__quotation__" + str(len(quotations) - 1) + english_file[end + 1:len(english_file)]
 
 print("quotations:", quotations)
-print("file lexed for quotations:", english_file)
+print("file lexed for quotations: '" + english_file + "'")
 
-# sentences = '.'.split(english_file)
+sentences = english_file.split('.')
+
+print("sentences:", sentences)
+
+
+def parse_sentence_object(object_to_parse: str):
+    if "__quotation__" in object_to_parse:
+        return quotations[int(object_to_parse.split("__quotation__")[1])]
+
+
+def execute(func, subject):
+    return func(subject)
+
+
+class Output:
+    @staticmethod
+    def says(text_to_say: str) -> str:
+        return "print('" + text_to_say + "')"
+
+
+common_nouns = {
+    "Output": {
+        "says": Output.says
+    }
+}
+
+output_lines = []
+for i in sentences:
+    if i == '':
+        continue
+    words = i.split(' ')
+    print("words:", words)
+
+    sentence_subject = words.pop(0)
+    sentence_verb = words.pop(0)
+    sentence_object = words.pop(0)
+
+    method_to_execute = common_nouns[sentence_subject][sentence_verb]
+    output_lines += [execute(method_to_execute, parse_sentence_object(sentence_object))]
+
+output.write('\n'.join(output_lines))
